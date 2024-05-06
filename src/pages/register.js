7532f73286ed1e30a4c40a71a226cc71a8181fe4
agooddogdance618 <zman.6.1.2020@gmail.com>
2024-05-06 17:51:00 -0400

    Database

--- src/pages/register.js
+++ src/pages/register.js
@@ -7,9 +7,10 @@ export default function register() {
       <div className='flex flex-col m-5 md:m-0 grow bg-white border-4 border-gray-700 max-w-screen-sm rounded-xl p-5'>
         <h1 className='text-center text-3xl font-semibold mb-5'>Sign Up</h1>
         <form className='flex flex-col items-center mb-5'>
-            <input className='w-1/2 p-1 duration-500 delay-150 scale-100 hover:scale-110 focus:scale-110 outline outline-2 rounded outline-gray-500 hover:outline-black focus:outline-black mb-3' placeholder='Email'/>
-            <input className='w-1/2 p-1 duration-500 delay-150 scale-100 hover:scale-110 focus:scale-110 outline outline-2 rounded outline-gray-500 hover:outline-black focus:outline-black mb-4' placeholder='Password'/>
-            <input className='w-1/2 p-1 duration-500 delay-150 scale-100 hover:scale-110 focus:scale-110 outline outline-2 rounded outline-gray-500 hover:outline-black focus:outline-black mb-4' placeholder='Confirm Password'/>
+            <input type='text' className='w-1/2 p-1 duration-500 delay-150 scale-100 hover:scale-110 focus:scale-110 outline outline-2 rounded outline-gray-500 hover:outline-black focus:outline-black mb-3' placeholder='Name'/>
+            <input type='text' className='w-1/2 p-1 duration-500 delay-150 scale-100 hover:scale-110 focus:scale-110 outline outline-2 rounded outline-gray-500 hover:outline-black focus:outline-black mb-3' placeholder='Email'/>
+            <input type='password' className='w-1/2 p-1 duration-500 delay-150 scale-100 hover:scale-110 focus:scale-110 outline outline-2 rounded outline-gray-500 hover:outline-black focus:outline-black mb-4' placeholder='Password'/>
+            <input type='password' className='w-1/2 p-1 duration-500 delay-150 scale-100 hover:scale-110 focus:scale-110 outline outline-2 rounded outline-gray-500 hover:outline-black focus:outline-black mb-4' placeholder='Confirm Password'/>
             <button className='w-1/3 duration-500 delay-150 scale-100 hover:scale-125 outline outline-2 outline-gray-500 hover:outline-black font-medium bg-green-500 p-1 rounded-full'>Sign Up</button>
         </form>
         <div className='text-center'>Already have an account? <Link className='underline' href="/signin">Sign In</Link></div>
